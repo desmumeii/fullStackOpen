4c2f941508ec1124107580c431c9cdccfc877025
Viet Anh Nguyen <anh.v.nguyen@tuni.fi>
2025-10-27 21:33:55 +0200

    Add some changes for step11 phonebookbackend

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 
 const app = express()
-
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
@@ -86,6 +86,6 @@ app.post('/api/persons', (request, response) => {
 const generateId = () => {
   return Math.floor(Math.random() * 10000).toString()
 }
-const PORT = 3001
+const PORT = process.env.PORT ||3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
